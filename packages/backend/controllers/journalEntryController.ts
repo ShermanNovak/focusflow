c94d25d8872ee64604ff2c8ca23cee4219197221
Ryan Loh <ryanloh28@yahoo.com.sg>
2023-06-09 11:38:36 +0800

    forgot to add files

---
+++ packages/backend/controllers/journalEntryController.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { Response } from "express";
+import { AuthenticatedRequest } from "../middleware/requireAuth";
+
+const JournalEntry = require('../models/JournalEntry');
+
+class JournalEntryController {
+    public async getJournalEntries (req: AuthenticatedRequest, res: Response) {
+        const journalEntries = await JournalEntry.find({ user: req.params.id }).sort({ createdAt: -1 });
+        res.json(journalEntries);
+    }
+
+    public async getJournalEntry (req: AuthenticatedRequest, res: Response) {
+        const entry = await JournalEntry.findById(req.params.id);
+        if (!entry) return res.status(404).json({ error: 'No such journal entry' });
+
+        res.json(entry);
+    }
+
+    public async createJournalEntry (req: AuthenticatedRequest, res: Response) {
+        try {
+            const entry = await JournalEntry.create({...req.body, user: req.params.id})
+            res.json(entry);
+        } catch (error: any) {
+            res.status(400).json({ error: error.message })
+        }
+    }
+
+    public async deleteJournalEntry (req: AuthenticatedRequest, res: Response) {
+        const entry = await JournalEntry.findOneAndDelete(req.params.id);
+        if (!entry) return res.status(404).json({ error: 'No such journal entry' });
+
+        res.json(entry);
+    }
+
+    public async updateJournalEntry (req: AuthenticatedRequest, res: Response) {
+        const entry = await JournalEntry.findOneAndUpdate(req.params.id, req.body);
+        if (!entry) return res.status(404).json({ error: 'No such journal entry' });
+
+        res.json(entry);
+    }
+}
+
+export const journalEntryController = new JournalEntryController();
