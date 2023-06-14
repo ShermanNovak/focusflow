fd29b4a4618343b60a4a8b7e24cff8eb40019791
ShermanNovak <sharlene@zumvet.com>
2023-06-14 22:29:45 +0800

    feat: added more links to readme

--- packages/frontend/src/pages/HomePage.tsx
+++ packages/frontend/src/pages/HomePage.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+import TaskModal from '../features/tasks/TaskModal';
+import TaskPanel from '../features/tasks/TaskPanel';
+
 export default function HomePage() {
+    const [showTaskModal, setShowTaskModal] = useState(false);
+    const [showTaskPanel, setShowTaskPanel] = useState(false);
+
     return (
       <>
+        {showTaskModal && <TaskModal />}
+        {showTaskPanel && <TaskPanel />}
       </>
     );
   }
