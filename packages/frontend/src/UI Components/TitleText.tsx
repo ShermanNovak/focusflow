28ae9cfb45a946b89199c9426c8d5ece8a602677
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2023-06-17 23:09:06 +0800

    added NavLink, Routes pls dont see card still wip

--- packages/frontend/src/UI Components/TitleText.tsx
+++ packages/frontend/src/UI Components/TitleText.tsx
@@ -4,7 +4,7 @@ interface InputProps {
   
   export default function TitleText(props: InputProps) {
     return (
-      <span className="text-sm pb-4 block ml-2">
+      <span className="text-sm pb-4 ml-2">
         {props.text}
       </span>
     );
