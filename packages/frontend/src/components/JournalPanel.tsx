a2e68e143dbcbf1f7a0a6523a3eed9469d4f8341
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2023-07-01 23:11:03 +0800

    adjusting formatting

--- packages/frontend/src/components/JournalPanel.tsx
+++ packages/frontend/src/components/JournalPanel.tsx
@@ -11,47 +11,46 @@ export default function JournalPanel() {
     const year = currentDate.getFullYear();
     const dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' }).toUpperCase();
 
-
     return (
         <RightPanel>
             <div className="absolute w-full left-0 h-1/4 top-0 bg-white flex">
-                    <div className="mx-auto my-auto">
-                        <DashedButton
-                            boldText="Add your Photo of the Day"
-                            text="What picture best represents your day?"
-                        >
-                        <CameraFilled className="text-2xl" />
-                        </DashedButton>
-                    </div>
+                <div className="mx-auto my-auto">
+                    <DashedButton
+                        boldText="Add your Photo of the Day"
+                        text="What picture best represents your day?"
+                    >
+                    <CameraFilled className="text-2xl" />
+                    </DashedButton>
                 </div>
-                <div className="absolute h-3/4 bottom-0">
-                    <div className="flex">
-                        <h3 className="mr-3 font-semibold">{day} · {month} · {year}</h3>
-                        <h3 className="font-semibold">|</h3>
-                        <h3 className="ml-3 font-semibold">{dayOfWeek}</h3>
-                    </div> 
-                    <Form
-                        labelAlign="left"
-                        labelCol={{ span: 5}}
-                        form={form}>
-                        <Form.Item name="journalTitle" rules={[{ required: true }]}>
-                            <Input.TextArea
-                                autoSize
-                                className="text-xl -ms-2 ps-2 text-black font-bold"
-                                placeholder="Insert Title Here"
-                                bordered={false}
-                            />
-                        </Form.Item>
-                        <Form.Item name="journalBody">
-                            <Input.TextArea
-                                    autoSize={{ minRows: 3}}
-                                    className="text-xl -ms-2 ps-2 text-black font-normal"
-                                    placeholder="Journal Entry"
-                                    bordered={false}
-                            />
-                        </Form.Item>
-                    </Form>
-                </div>
+            </div>
+            <div className="absolute h-3/4 bottom-0">
+                <div className="flex">
+                    <h3 className="mr-3 font-semibold">{day} · {month} · {year}</h3>
+                    <h3 className="font-semibold">|</h3>
+                    <h3 className="ml-3 font-semibold">{dayOfWeek}</h3>
+                </div> 
+                <Form
+                    labelAlign="left"
+                    labelCol={{ span: 5}}
+                    form={form}>
+                    <Form.Item name="journalTitle" rules={[{ required: true }]}>
+                        <Input.TextArea
+                            autoSize
+                            className="text-xl -ms-2 ps-2 text-black font-bold"
+                            placeholder="Insert Title Here"
+                            bordered={false}
+                        />
+                    </Form.Item>
+                    <Form.Item name="journalBody">
+                        <Input.TextArea
+                            autoSize={{ minRows: 3}}
+                            className="text-xl -ms-2 ps-2 text-black font-normal"
+                            placeholder="Journal Entry"
+                            bordered={false}
+                        />
+                    </Form.Item>
+                </Form>
+            </div>
         </RightPanel>
     )
 }
