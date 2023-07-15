191b9fbf0136fb937e9b631eb7d2b7d5e1281b86
ShermanNovak <sharlene@zumvet.com>
2023-07-15 08:50:47 +0100

    feat: added break mode to pomodoro

--- packages/frontend/src/api/axios.tsx
+++ packages/frontend/src/api/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // change token variable to user context token
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDdjOWIyMjE0NmE2MjJhYmRkMDhmYmIiLCJpYXQiOjE2ODkxNTM0NTUsImV4cCI6MTY4OTQxMjY1NX0.kahCm2I8x_5vO8ShpsSOX2a_qFHPkjgV4-Fo8pSZLs0";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDdjOWIyMjE0NmE2MjJhYmRkMDhmYmIiLCJpYXQiOjE2ODk0MDcwOTcsImV4cCI6MTY4OTY2NjI5N30.uEiOrDpTWO51_DrpEMk_sWIhhxuMsq7jrCZIdFd_SIs";
 
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:3001/",
