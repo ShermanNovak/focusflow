b540bdc7dd7da0c1b9eba803af5bfb39cecfa288
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2023-07-24 10:48:08 +0800

    highlight (backend)

---
+++ packages/backend/routes/highlightRoutes.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { requireAuth } from '../middleware/requireAuth'
+
+const { highlightController } = require("../controllers/highlightController");
+
+const router = express.Router();
+
+router.use(requireAuth); // requires user to sign in
+
+router.post("/", highlightController.createHighlight);
+router.get("/", highlightController.getHighlight); //-> filtered by that particular day to display the highlight
+router.patch('/', highlightController.updateHighlight); //-> filtered by date
+
+export default router;
