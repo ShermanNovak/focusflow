56916180504979445583823b243dbf0ebbb06630
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2023-08-03 16:29:13 +0800

    feat: highlight able to view on frontend

--- packages/frontend/src/pages/HomePage.tsx
+++ packages/frontend/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { KeyboardEvent } from "react";
 import { Input, Form } from "antd";
 import { useTaskCreation } from "../api/tasks.query";
 import { useHighlightCreation } from "../api/highlight.query";
+import { useHighlightQuery } from "../api/highlight.query";
 
 import SmallCaps from "../components/SmallCaps";
 
@@ -39,6 +40,8 @@ export default function HomePage() {
       }
     }
   }
+  const { data: highlightData, isLoading: highlightIsLoading } = useHighlightQuery(); // fetching data from prev highlight entry 
+
   
     const [showTaskModal, setShowTaskModal] = useState(false);
     const [showTaskPanel, setShowTaskPanel] = useState(true);
@@ -52,26 +55,32 @@ export default function HomePage() {
       </span>
       <div className="pt-3 grid grid-cols-2 gap-4">
         <div>
-          <Form form={highlightForm}>
-            <div className="flex gap-x-1 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <SmallCaps text="WHAT IS YOUR HIGHLIGHT OF THE DAY?" />
-            </div>
-            <Form.Item name="highlight">
+          <div className="flex gap-x-1 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <SmallCaps text="WHAT IS YOUR HIGHLIGHT OF THE DAY?" />
+          </div>
+          {highlightIsLoading && <h6>Fetching Higlight Entry...</h6>}
+          {!highlightIsLoading && (
+          <Form 
+            form={highlightForm}
+            initialValues={highlightData}
+            >
+            <Form.Item name="content">
               <Input className="bg-pale-yellow" onKeyDown={createHighlightHandler}/>
             </Form.Item>
           </Form>
+          )}
           <SmallCaps text="HERE IS YOUR SCHEDULE FOR TODAY 💪" />
         </div>
         <div>
