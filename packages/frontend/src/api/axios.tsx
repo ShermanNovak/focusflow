08b1932e3d1a1b22b5d7675d6bb1d2049fa1d1bc
Ryan Loh <ryanloh28@yahoo.com.sg>
2023-08-12 22:14:37 +0800

    changed baseurl to env var

--- packages/frontend/src/api/axios.tsx
+++ packages/frontend/src/api/axios.tsx
@@ -4,8 +4,8 @@ import axios from "axios";
 let TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDdjOWIyMjE0NmE2MjJhYmRkMDhmYmIiLCJpYXQiOjE2OTE2ODEwNDksImV4cCI6MTY5MTk0MDI0OX0.WQmLWrRAgXSE7kKwh81D3Sp58hilXo_DxH4GXeFKnaM";
 
-  export const axiosInstance = axios.create({
-  baseURL: "http://localhost:80/",
+export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${TOKEN}`,
@@ -13,7 +13,7 @@ let TOKEN =
 });
 
 export const axiosImageInstance = axios.create({
-  baseURL: "http://localhost:80/api/images/",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "multipart/form-data",
     Authorization: `Bearer ${TOKEN}`,
